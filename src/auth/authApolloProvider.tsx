5572e3c743e09b4e12533a782973cac2f0af93fe
Jose Alberto Ch <jose.charana@globant.com>
2022-06-03 12:17:38 -0500

    added uri backend directly in apollo driver

--- src/auth/authApolloProvider.tsx
+++ src/auth/authApolloProvider.tsx
@@ -9,33 +9,34 @@ import { getMainDefinition } from '@apollo/client/utilities';
 const AuthApolloProvider = ({ children }: { children: React.ReactNode }) => {
 
   const uploadLink = createUploadLink({
-    uri: process.env.REACT_APP_API!,
+    uri: 'http://3.135.81.129/graphql'
+    // uri: process.env.REACT_APP_API!,
   });
 
-  const wsLink = new GraphQLWsLink(createClient({
-    url: 'ws://localhost:3000/graphql',
-    // url: 'http://localhost:3000/graphql',
-  }));
+  // const wsLink = new GraphQLWsLink(createClient({
+  //   url: 'ws://localhost:3000/graphql',
+  //   // url: 'http://localhost:3000/graphql',
+  // }));
 
   // The split function takes three parameters:
   //
   // * A function that's called for each operation to execute
   // * The Link to use for an operation if the function returns a "truthy" value
   // * The Link to use for an operation if the function returns a "falsy" value
-  const splitLink = split(
-    ({ query }) => {
-      const definition = getMainDefinition(query);
-      return (
-        definition.kind === 'OperationDefinition' &&
-        definition.operation === 'subscription'
-      );
-    },
-    wsLink,
-    uploadLink,
-  );
+  // const splitLink = split(
+  //   ({ query }) => {
+  //     const definition = getMainDefinition(query);
+  //     return (
+  //       definition.kind === 'OperationDefinition' &&
+  //       definition.operation === 'subscription'
+  //     );
+  //   },
+  //   // wsLink,
+  //   uploadLink,
+  // );
 
   const apolloClient = new ApolloClient({
-    link: splitLink,
+    link: uploadLink,
     cache: new InMemoryCache(),
   });
 
